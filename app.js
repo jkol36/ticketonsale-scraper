0f759c0e80714d6e98f5bf7dc8cc64e892a462c9
Jon Kolman <jonathankolman@gmail.com>
2017-07-22 08:47:05 -0400

    run every 24 hours

--- app.js
+++ app.js
@@ -9,19 +9,11 @@ import {
  import { signalingRef } from './config'
 
  const start = () => {
-  console.log('ticket on sale scraper waiting to run')
-  signalingRef.on('child_added', s => {
-    console.log('ticket on sale scraper running')
-    //remove the signal once scraper runs
-    let key = s.key
-    loginToTicketOnSale()
-    .then(getSalesList)
-    .then(parseSalesPage)
-    .then(saveEventList)
-    .then(() => signalingRef.child(key).remove())
-    .catch(console.log)
-
-  })
+  return loginToTicketOnSale()
+  .then(getSalesList)
+  .then(parseSalesPage)
+  .then(saveEventList)
+  .catch(console.log)
  }
 
- start()
+setInterval(() => start(), 86400000)
